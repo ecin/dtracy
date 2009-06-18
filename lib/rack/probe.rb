6dcf8d69a919c63299ea1bfa3677c81faa92f8d2
ecin <ecin@copypastel.com>
2009-06-18 05:21:25 -0400

    Added PID logging to @@events hashes, small code cleanup as well.

--- lib/rack/probe.rb
+++ lib/rack/probe.rb
@@ -5,7 +5,7 @@ module Rack
     require 'thread'
 
     @@events = []
-    
+
     def self.events
       @@events
     end
@@ -16,10 +16,13 @@ module Rack
       dtracy = Dtrace.new
       dtracy.setopt('bufsize', '8m')
 
-      # Get code from a file, can be set with initialize
-      code = ':::function-entry
+      # TODO: Get code from a file, can be set with initialize
+      # Hardcoded for now
+      code = <<-DSCRIPT
+      :::function-entry
       /copyinstr((int) arg0) == "Rack::Probe" && copyinstr((int) arg1) == "call"/
       {
+        trace(pid);
         trace(probeprov);
         trace(probemod);
         trace(probefunc);
@@ -27,35 +30,42 @@ module Rack
         trace(copyinstr((int) arg0));
         trace(copyinstr((int) arg1));
         trace(walltimestamp); /* Seems the resulting integer is too large for Ruby */
-        }'
+      }
+      DSCRIPT
 
-        dtracy.compile(code).execute
-        dtracy.go 
+      dtracy.compile(code).execute
+      dtracy.go 
 
-        dtracy.buf_consumer(nil)
+      dtracy.buf_consumer(nil)
 
-        Thread.new do
-          while true
-            begin 
-              dtracy.work( proc do |data|  
-                result = []
-                result << data.records[0..3].map{|r| r.value}.join(':')
-                result << data.records[4..-1].map{|r| r.value}
-                result.flatten!
-                @@events << { :probe => result[0], :arguments => result[1..-2], :timestamp => result[-1] }
-              end)
-            rescue
-              sleep 1
-              retry
-            end
+      Thread.new do
+        while true
+          begin 
+            dtracy.work( proc do |data|  
+              result = []
+              # Push in the PID
+              result << data.records[0].value
+              # Push in the 4-tuple name for the probe
+              result << data.records[1..4].map{|r| r.value}.join(':')
+              # Anything that remains is an argument for said probe
+              # Timestamp is tacked at the end
+              result << data.records[5..-1].map{|r| r.value}
+              result.flatten!
+              @@events << { :pid => result[0], :probe => result[1], :arguments => result[2..-2], :timestamp => result[-1] }
+            end)
+          rescue Exception => e
+            puts e
+            sleep 1
+            retry
           end
         end
       end
-
-
-      def call( env )
-        @app.call env
-      end
-
     end
+
+
+    def call( env )
+      @app.call env
+    end
+
   end
+end
