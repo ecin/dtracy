9af4d82cfb66667649bfa35b364e270f14488eeb
ecin <ecin@copypastel.com>
2009-07-07 11:18:39 -0400

    Fixed probe's first argument not being stored due to an off-by-one index.

--- lib/sinatra/dtracy.rb
+++ lib/sinatra/dtracy.rb
@@ -23,8 +23,8 @@ module Sinatra
     # TODO: Get code from a file, can be set with initialize
     # Hardcoded for now
     code = <<-DSCRIPT
-    :::request
-    /copyinstr((int) arg0) == "/push"/
+    :::path
+    /copyinstr((int) arg0) != "/updates"/
     {
       trace(pid);
       trace(probemod);
@@ -43,16 +43,15 @@ module Sinatra
     Thread.new do
       loop do
         dtracy.work( proc do |data|  
-          result = []
           # Push in the PID
-          result << data.records[0].value
+          pid = data.records[0].value
           # Push in the 4-tuple name for the probe
-          result << data.records[1..3].map{|r| r.value}.join(':')
+          name = data.records[1..3].map{|r| r.value}.join(':')
           # Anything that remains is an argument for said probe
+          args = data.records[4..-2].map{|r| r.value}
           # Timestamp is tacked at the end
-          result << data.records[5..-1].map{|r| r.value}
-          result.flatten!
-          @@events << { :pid => result[0], :probe => result[1], :arguments => result[2..-2], :timestamp => result[-1] }
+          ts = data.records[-1].value
+          @@events << { :pid => pid, :probe => name, :arguments => args, :timestamp => ts }
         end)
       end
     end
