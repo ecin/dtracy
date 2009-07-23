293ba4456cc360449533c67dbb5fcad30c36754f
ecin <ecin@copypastel.com>
2009-07-22 21:53:54 -0400

    Dtrace consumers are now started in a new process instead of in a new thread. Big win.

--- lib/sinatra/dtracy.rb
+++ lib/sinatra/dtracy.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'haml'
 require 'rack/contrib'
 require 'json'
+require 'drb'
 
 gem 'ruby-dtrace', '>= 0.2.7'
 require 'dtrace'
@@ -16,62 +17,71 @@ module Sinatra
 
     use Rack::JSONP
     use Rack::Probe
-
-    @@events = []
-    @@probes = []
     
-    dtracy = Dtrace.new
-    dtracy.setopt('bufsize', '8m')
-
     # TODO: Get code from a file, can be set with initialize
     # Hardcoded for now
     code = <<-DSCRIPT
     :::path
     /copyinstr((int) arg0) != "/updates"/
     {
-      trace(pid);
-      trace(probemod);
-      trace(probefunc);
-      trace(probename); /* A given for all probes here */
-      trace(copyinstr((int) arg0));
       trace(walltimestamp); /* Seems the resulting integer is too large for Ruby */
+      trace(copyinstr((int) arg0));
     }
     DSCRIPT
 
-    program = dtracy.compile(code)
-    dtracy.each_probe_prog(program) do |probe|
-      @@probes << probe.to_s
-    end
-    
-    program.execute
-    dtracy.go 
+    class Dtective
+      @@probes = []
+      @@events = []
+      @@bin = File.expand_path(File.dirname(__FILE__)+'/bin/dtective')
+      @@port = 65000
 
-    dtracy.buf_consumer(nil)
+      def self.probes
+        @@probes
+      end
+      
+      def self.events
+        @@events
+      end
 
-    Thread.new do
-      loop do
-        dtracy.work( proc do |data|  
-          # Push in the PID
-          pid = data.records[0].value
-          # Push in the 4-tuple name for the probe
-          name = data.records[1..3].map{|r| r.value}.join(':')
-          # Anything that remains is an argument for said probe
-          args = data.records[4..-2].map{|r| r.value}
-          # Timestamp is tacked at the end
-          ts = data.records[-1].value
-          @@events << { :pid => pid, :probe => name, :arguments => args, :timestamp => ts }
-        end)
+      attr_reader :script
+
+      DRb.start_service
+
+      def initialize(script)
+        @script = script
+        @port = @@port
+        DRb.start_service "druby://localhost:#{@port}", self
+        @@port += 1
+      end
+
+      def run
+        IO.popen("#{@@bin} #{@port}")
+        puts "[ Dtracy ] Dtective reporting on port #{@port}"
+      end
+
+      def inform( events )
+        @@events << events
+      end
+
+      def events
+        @@events
+      end
+
+      def add_probe( probe )
+        @@probes << probe
       end
     end
-
+    
+    Dtective.new(code).run
+    
     get '/' do
-      @probes = @@probes
+      @probes = Dtective.probes
       haml :index
     end
 
     get '/updates' do
       content_type :json
-      events = @@events
+      events = Dtective.events
       idx = params[:idx].to_i
       sleep 1 while idx >= (length = events.length) # Take naps until there's new data
       events = events[idx..-1] || []
